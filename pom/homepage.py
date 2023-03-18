ed63133436c9357443b4e024090341dba92ecfa2
osipov.a <osipov.workmail@gmail.com>
2023-03-18 17:32:46 +0600

    modified

--- pom/homepage.py
+++ pom/homepage.py
@@ -6,24 +6,21 @@ from typing import List
 class HomepageNav(SeleniumBase):
 
     # CSS locators
-    categories = '.c_320-menu-links-list'
-    category = '.c_320-build_cta-long'
+    nav_links = '.c_320-menu-links-list[role="menubar"]'
+    build_price_link = '.c_320-build_cta-long'
 
     # Return all categories WebElements except Build_And_Price
     def get_nav_links(self) -> List[WebElement]:
-        return self.are_visible('css', self.categories, 'Vehicles,Shop At Home,Discover Nissan,Owners,Locate Dealer')
+        return self.are_visible('css', self.nav_links, 'Vehicles,Shop At Home,Discover Nissan,Owners,Locate Dealer')
 
     # Return a Build_And_Price WebElement
-    def get_category(self) -> WebElement:
-        return self.is_visible('css', self.category, 'Build & Price')
+    def get_build_price_link(self) -> WebElement:
+        return self.is_visible('css', self.build_price_link, 'Build & Price')
 
-    # Methods for getting the text of each title button
+    # Method for getting the text of each title button
     def get_nav_links_text(self) -> List[str]:
-        nav_links = self.get_nav_links()
-        nav_links_text = [link.text for link in nav_links]
-        return ",".join(nav_links_text).split('\n')
+        return self.get_nav_links()[-1].text.split('\n')
 
     # Method for getting the text of Build & Price button
-    def get_nav_link_text(self) -> str:
-        nav_link_text = self.get_category().text
-        return nav_link_text
+    def get_build_price_text(self) -> str:
+        return self.get_build_price_link().text
--- tests/test_homepage.py
+++ tests/test_homepage.py
@@ -1,14 +1,16 @@
 import pytest
 from pom.homepage import HomepageNav
 
+expected_nav_links_value = ['Vehicles', 'Shop At Home', 'Discover Nissan', 'Owners', 'Locate Dealer']
+expected_build_price_value = 'Build & Price'
+
 
 @pytest.mark.usefixtures('setup')
 class TestHomepage:
 
     def test_homepage_text(self):
         homepage = HomepageNav(self.driver)
-        verify_categories = 'Vehicles,Shop At Home,Discover Nissan,Owners,Locate Dealer'
-        verify_category = 'Build & Price'
-        actual_links = (",".join(homepage.get_nav_links_text()), homepage.get_nav_link_text())
-        expected_links = (verify_categories, verify_category)
-        assert actual_links == expected_links, 'Validating Nav Links text'
+        actual_nav_links = homepage.get_nav_links_text()
+        actual_build_price = homepage.get_build_price_text()
+        assert actual_nav_links == expected_nav_links_value, 'Validating Nav Links text'
+        assert actual_build_price == expected_build_price_value, 'Validating Build & Price text'
