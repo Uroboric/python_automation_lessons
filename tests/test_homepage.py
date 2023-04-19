db24b6af9eff841a775ec0cdbd24b39f0fa8c4db
osipov.a <osipov.workmail@gmail.com>
2023-04-20 04:41:58 +0600

    add scrolling,clicking and using pytest-check

--- tests/test_homepage.py
+++ tests/test_homepage.py
@@ -1,16 +1,42 @@
 import pytest
 from pom.homepage import HomepageNav
-
-expected_nav_links_value = ['Vehicles', 'Shop At Home', 'Discover Nissan', 'Owners', 'Locate Dealer']
-expected_build_price_value = 'Build & Price'
+from pytest_check import check
 
 
 @pytest.mark.usefixtures('setup')
 class TestHomepage:
 
-    def test_homepage_text(self):
+    def test_homepage_tabs(self):
+        # crossovers_and_SUVs
         homepage = HomepageNav(self.driver)
-        actual_nav_links = homepage.get_nav_links_text()
-        actual_build_price = homepage.get_build_price_text()
-        assert actual_nav_links == expected_nav_links_value, 'Validating Nav Links text'
-        assert actual_build_price == expected_build_price_value, 'Validating Build & Price text'
+        num_tab_crossovers_and_SUVs = int(homepage.get_category_crossovers_and_SUVs_text())
+        num_grid_pannel_crossovers_and_SUVs = homepage.get_num_crossovers_and_SUVs()
+        check.equal(num_tab_crossovers_and_SUVs, num_grid_pannel_crossovers_and_SUVs)
+
+        # Cars
+        self.driver.execute_script("return arguments[0].scrollIntoView(true);", homepage.get_click_cars())
+        homepage.get_click_cars().click()
+        num_tab_cars = int(homepage.get_category_cars_text())
+        num_grid_pannel_cars = homepage.get_num_cars()
+        check.equal(num_tab_cars, num_grid_pannel_cars)
+
+        # Electric
+        self.driver.execute_script("return arguments[0].scrollIntoView(true);", homepage.get_click_electric())
+        homepage.get_click_electric().click()
+        num_tab_electric = int(homepage.get_category_electric_text())
+        num_grid_pannel_electric = homepage.get_num_electric()
+        check.equal(num_tab_electric, num_grid_pannel_electric)
+
+        # Sports cars
+        self.driver.execute_script("return arguments[0].scrollIntoView(true);", homepage.get_click_sports_cars())
+        homepage.get_click_sports_cars().click()
+        num_tab_sports_cars = int(homepage.get_category_sports_cars_text())
+        num_grid_pannel_sports_cars = homepage.get_num_sports_cars()
+        check.equal(num_tab_sports_cars, num_grid_pannel_sports_cars)
+
+        # Trucks
+        self.driver.execute_script("return arguments[0].scrollIntoView(true);", homepage.get_click_trucks())
+        homepage.get_click_trucks().click()
+        num_tab_trucks = int(homepage.get_category_trucks_text())
+        num_grid_pannel_trucks = homepage.get_num_trucks()
+        check.equal(num_tab_trucks, num_grid_pannel_trucks)
