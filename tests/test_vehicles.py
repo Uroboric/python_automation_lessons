10016859a6f932daad2bbbb735fc3d09f41924cb
osipov.a <98850554+Uroboric@users.noreply.github.com>
2023-10-08 01:33:51 +0600

    update (#6)

--- tests/test_vehicles.py
+++ tests/test_vehicles.py
@@ -1,13 +1,16 @@
+import allure
 import pytest
 from pom.vehicles import VehiclesNav
 
 
+@allure.suite("Testing Vehicles parametrize")
 @pytest.mark.usefixtures('setup')
 @pytest.mark.parallel
 class TestVehicles:
-    test_data = [('Ariya', '$43,190'), ('Kicks', '$20,440'), ('Rogue Sport', '$24,960'), ('Rogue', '$27,760'),
-                 ('Murano', '$34,160'), ('Pathfinder', '$35,200'), ('Armada', '$50,700')]
+    test_data = [('Ariya', '$43,190'), ('Kicks', '$20,440'), ('Rogue Sport', '$24,960'), ('Rogue', '$27,910'),
+                 ('Murano', '$34,160'), ('Pathfinder', '$35,320'), ('Armada', '$50,700')]
 
+    @allure.title("Asserting Vehicles prices")
     @pytest.mark.parametrize('car_name, expected_price', test_data)
     def test_vehicle_price(self, car_name, expected_price):
         vehicles = VehiclesNav(self.driver)
