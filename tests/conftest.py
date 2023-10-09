9c000fde25c9b6baeead8b69778dfef5e1bf9c34
osipov.a <98850554+Uroboric@users.noreply.github.com>
2023-10-09 15:47:16 +0600

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -25,7 +25,7 @@ def get_chrome_options():
 @pytest.fixture
 def get_webdriver(get_chrome_options):
     options = get_chrome_options
-    driver = webdriver.Chrome(service=ChromiumService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()))
+    driver = webdriver.Chrome(service=ChromiumService(ChromeDriverManager(version="117.0.5938.132",chrome_type=ChromeType.CHROMIUM).install()))
     return driver
 
 
